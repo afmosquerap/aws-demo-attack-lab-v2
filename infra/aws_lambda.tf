c78f8d971daa0621d4d728c1464dc09e7efee603
afmosquerap <afmosquerap@udistrital.edu.co>
2024-08-26 11:07:17 -0500

    Update aws_lambda.tf

--- infra/aws_lambda.tf
+++ infra/aws_lambda.tf
@@ -31,7 +31,7 @@ resource "aws_lambda_function" "analysis_lambda" {
 
   environment {
     variables = {
-      access_key = "AKIAIOSFODNN7EXAMPLE12"
+      access_key = "AKIAIOSFODNN7EXAMPLE1"
       secret_key = "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY"
     }
   }
